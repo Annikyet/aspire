c199b3221f83503a1c60068b4428ed48ea29367d
annikyet <annikyet@icloud.com>
2022-10-11 11:39:24 -0700

    functional tutorial app

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ class Board extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      squares: Array(9).fill('O')
+      squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
   renderSquare(i) {
@@ -31,12 +32,22 @@ class Board extends React.Component {
   handleClick(i) {
     // console.log(`You clicked ${i}`)
     const squares = this.state.squares.slice()  // create a clone of the array
-    squares[i] = 'X'
-    this.setState({squares: squares})
+    if (!squares[i] && !calculateWinner(this.state.squares)) {
+      squares[i] = this.state.xIsNext ? 'X' : 'O'
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext})
+    }
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status
+    if (winner) {
+      status = `Winner: ${winner}!!!`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
 
     return (
       <div>
@@ -75,6 +86,31 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (
+      squares[a] &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c]
+    ) {
+      return squares[a]
+    }
+  }
+  return null
 }
 
 // ========================================
