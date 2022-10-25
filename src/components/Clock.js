641bef6f7691ef0ea5942b87d89c1bea09a438c2
annikyet <annikyet@icloud.com>
2022-10-25 13:13:23 -0700

    working clock

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// TODO make the clock auto-update
+// TODO allow clock format to be changed
+// TODO allow clock styling to be changed
 export class Clock extends React.Component {
   render() {
+    let now = new Date()
+
+    // format mins int to 0-padded 2 digit string
+    let mins = now.getMinutes()
+    let minStr = mins.toString()
+    if (mins < 10) {
+      minStr = '0' + minStr
+    }
+
+    let timeStr = `${now.getHours()}:${minStr}`
+
     return (
-      <div className="mt-[24vh] bg-purple-500">clock</div>
+      <div className="mt-[24vh] p-8 text-center text-8xl bg-purple-500">{timeStr}</div>
     )
   }
 }
