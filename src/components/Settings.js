b77e71c5577b710f3fb6a333185eddb001ebcc7b
annikyet <annikyet@icloud.com>
2022-10-25 13:27:09 -0700

    settings button

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// TODO Settings button should open settings pane
 export class SettingsBtn extends React.Component {
   render() {
     return (
-      <div className="bg-blue-400 justify-self-end">[]</div>
+      <div className="hover:bg-slate-300 justify-self-end p-2">
+        <i className="mdi mdi-menu"></i>
+      </div>
     )
   }
 }
 
+// TODO actually put stuff here
 export class SettingsPane extends React.Component {
   render() {
     return (
