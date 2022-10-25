07a18b0138d1b5fdcf3405bdc282477eca513ff3
annikyet <annikyet@icloud.com>
2022-10-25 12:22:47 -0700

    more wireframing

--- src/index.js
+++ src/index.js
@@ -3,18 +3,26 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 
-class Game extends React.Component {
+class Main extends React.Component {
   render() {
     return (
-      <header className="grid grid-cols-4">
-        <section className="col-1 bg-red-400">todoBar</section>
-        <section className="col-start-4 col-1 flex">
-          <section className="bg-green-400 grow">weatherBar</section>
-          <section className="bg-blue-400 justify-self-end">settingsBtn</section>
-        </section>
-      </header>
+      <main className="flex flex-col">
+        <div className="flex justify-between">
+          <div className="min-w-[25vw] bg-red-400">todos</div>
+          <div className="min-w-[25vw] flex">
+            <div className="bg-green-400 grow">weather</div>
+            <div className="bg-blue-400 justify-self-end">[]</div>
+          </div>
+        </div>
+        
+        <div className="flex">
+          <div className="hidden md:w-[25vw] bg-purple-500">
+            hewwo
+          </div>
+        </div>
+      </main>
 
-      
+
     );
   }
 }
@@ -22,5 +30,4 @@ class Game extends React.Component {
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
-
+root.render(<Main />);
