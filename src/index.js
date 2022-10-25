ea65df1dc4bcae7037d64d90519acc57f93e796f
annikyet <annikyet@icloud.com>
2022-10-25 12:42:48 -0700

    started building components

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+// import components
+import TodoBar from './components/TodoBar.js'
+
 
 class Main extends React.Component {
   render() {
     return (
       <main className="flex flex-col h-[100vh]">
         <div className="flex justify-between">
-          <div className="min-w-[25vw] bg-red-400">todos</div>
+          <TodoBar />
           <div className="min-w-[25vw] flex">
             <div className="bg-green-400 grow">weather</div>
             <div className="bg-blue-400 justify-self-end">[]</div>
